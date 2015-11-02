64d1a1f16f3114933d7d2eb3a32443b49d31b637
Jan Ypma <jyp@tradeshift.com>
2015-11-02 13:42:33 +0100

    release 0.1-201511021334

--- build.sbt
+++ build.sbt
@@ -2,3 +2,5 @@ name := "scala-japi"
 
 organization := "com.tradeshift"
 
+// The root isn't published, only submodules are
+publishArtifact := false
--- project/ScalaJApi.scala
+++ project/ScalaJApi.scala
@@ -5,8 +5,10 @@ import com.typesafe.sbteclipse.plugin.EclipsePlugin._
 object ScalaJApi extends Build {
   lazy val commonSettings = Seq(
     organization := "com.tradeshift.scala-japi",
-    version := "0.1-SNAPSHOT",
+    version := "0.1-201511021334",
     scalaVersion := "2.11.7",
+    scalacOptions ++= "-deprecation" :: "-feature" :: "-target:jvm-1.8" :: Nil,
+    licenses := Seq(("MIT", url("http://opensource.org/licenses/MIT"))),
     EclipseKeys.executionEnvironment := Some(EclipseExecutionEnvironment.JavaSE18),
     EclipseKeys.withSource := true
   )
